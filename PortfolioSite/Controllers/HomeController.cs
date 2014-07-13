019ffa2e98563ab4e689ce5a6eeece8fc3b0d8dd
Kyle Wood <kyle.c.wood88@gmail.com>
2014-07-13 15:21:54 -0600

    PortfolioSite
    Changes that I made

---
+++ PortfolioSite/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Home/About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: /Home/Work
+        public ActionResult Work()
+        {
+            return View();
+        }
+
+        // GET: /Home/Contact 
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            //Create a new contact form object
+            // pass to our view
+            Models.ContactForm cf = new Models.ContactForm();
+            return View(cf);
+        }
+
+        // POST: /Home/Contact
+        [HttpPost]
+        public ActionResult Contact(Models.ContactForm cf)
+        {
+            //Create a new connection to our database
+            Models.ContactFormEntities db = new Models.ContactFormEntities();
+            //Add the contact info to the database
+            db.ContactForms.Add(cf);
+            //save changes to the database
+            db.SaveChanges();
+            //Redirect user to the thank you page
+            return RedirectToAction("ThankYou");
+        }
+
+        // GET: /Home/ThankYou
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioSite/Models/ContactFormValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Add using statement for data annotations
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioSite.Models
+{
+    //Add the metadata type to the contact form
+    [MetadataType(typeof(ContactFormValidation))]
+    public partial class ContactForm
+    {
+
+    }
+
+    public class ContactFormValidation
+    {
+        //Data annotations hanf the label display of our 
+        // fields and set the requirments for valid data. 
+        [Required(ErrorMessage="The Daleks will exterminate you if you do not enter a name!"), Display(Name="Your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage="How can I connect you with other Whovians at your company if I don't know who you work for?")]
+        public string CompanyName { get; set; }
+        [Required, EmailAddress(ErrorMessage = "All Whovians have valid email messages!"), MaxLength(50)]
+        public string Email { get; set; }
+        [Required(ErrorMessage="I can not connect you with further Whovians if you do not leave a comment!"), DataType(DataType.MultilineText),MaxLength(500)]
+        public string Comments { get; set; }
+    }
+
+}
